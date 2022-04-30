648ff38320dcea40c746d6d578cd00b5042af12d
Trinity Gao <ngao@ucsd.edu>
2022-04-30 01:21:13 -0700

    add test

--- ILove.java
+++ ILove.java
@@ -1,4 +1,9 @@
 public class ILove{
 
+    public static String ILove(String fav) {
+        String Love = "I love " + fav + " so much !";
+
+        return Love;
+    }
 
 }
---
+++ ILoveTester.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ILoveTester {
+
+    @Test
+    public void Tester () {
+        assertEquals("I love you so much !!", ILove.ILove("you")); // test should fail
+
+    }
+
+}
