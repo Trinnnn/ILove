748b3b9be18d2181b80bfd7bede841d41c94b7c1
Trinity Gao <ngao@ucsd.edu>
2022-05-05 16:16:16 -0700

    Update ILoveTester.java

--- ILoveTester.java
+++ ILoveTester.java
@@ -4,9 +4,13 @@ import org.junit.*;
 public class ILoveTester {
 
     @Test
-    public void Tester () {
+    public void Tester1() {
         assertEquals("I love you so much !", ILove.ILove("you")); // test should fail
+    }
 
+    @Test
+    public void Tester2() {
+        assertEquals("I love Bucky so much !", ILove.ILove("Bucky")); // test should fail
     }
 
 }
